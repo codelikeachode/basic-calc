f4dfc99b3fc0aeb40dab1dd3b21c8eb8b34703d1
codelikeachode <derpypj@gmail.com>
2023-05-16 21:33:28 -0400

    add ref. calc elements, and variables for calc.

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-document.addEventListener('DOMContentLoaded', function () {
-    
-})
+// Wait for the HTML document to load
+document.addEventListener("DOMContentLoaded", function () {
+	// Get references to the calculator elements
+	const resultElement = document.getElementById("result");
+	const clearButton = document.querySelector(".clear");
+	const operatorButtons = document.querySelectorAll(".operator");
+	const numberButtons = document.querySelectorAll(".number");
+	const equalsButton = document.querySelector(".equals");
+	const decimalButton = document.querySelector(".decimal");
+
+	// Variables to store calculator state
+    let firstOperand = null;
+    let operator = null;
+    let shouldClearResult = false;
+
+    // Function to update the displayed result
+});
